222613bc469e04d47f57f31edb76e9061710f398
maxkepter <tuna26hn@gmail.com>
2026-07-16 09:20:58 +0700

    feat: update app module to include new feature modules and add docker-compose for SQL Server setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomerModule } from './modules/customer/customer.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
+import { CustomersModule } from './modules/customers/customers.module';
 import { DatabaseModule } from './modules/database/database.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { RoomsModule } from './modules/rooms/rooms.module';
+import { ServicesModule } from './modules/services/services.module';
 
 @Module({
-  imports: [DatabaseModule, CustomerModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    BookingsModule,
+    CustomersModule,
+    PaymentsModule,
+    RoomsModule,
+    ServicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
